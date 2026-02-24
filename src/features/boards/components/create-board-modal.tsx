3190ffe92619607a99c340b1c1e35c79a3001ba9
Andrey Yurev <ayurev.work@mail.ru>
2026-02-24 19:08:37 +0800

    feat(boards): add BOARD_COLORS constant and CreateBoardModal skeleton

--- src/features/boards/components/create-board-modal.tsx
+++ src/features/boards/components/create-board-modal.tsx
@@ -1,6 +1,14 @@
 'use client'
 
 import { useUIStore } from '@/store/ui.store'
+import * as z from 'zod'
+import { BOARD_COLORS } from '../constants'
+
+export const BoardSchema = z.object({
+  title: z.string().min(1, 'Введите название доски'),
+  description: z.string().optional(),
+  color: z.enum(BOARD_COLORS).default(BOARD_COLORS[0]),
+})
 
 export default function CreateBoardModal() {
   const isOpen = useUIStore((state) => state.isCreateBoardModalOpen)
---
+++ src/features/boards/constants.ts
@@ -0,0 +1,10 @@
+export const BOARD_COLORS = [
+  '#F54927',
+  '#27D3F5',
+  '#65F527',
+  '#B727F5',
+  '#F52727',
+  '#27F5F5',
+  '#61F527',
+  '#BB27F5',
+] as const
